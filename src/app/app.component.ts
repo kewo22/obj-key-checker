5eb4ed179660e1e945f4b3062eb8feee1ec5bda3
kewo22 <kewinf271@gmail.com>
2019-05-08 12:24:54 +0530

    check type init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,20 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     // console.log(sample)
     // console.log(sample2)
-    
+
     for (const i in Object.keys(sample)) {
-      console.log(Object.keys(sample)[i] + ' ----- ' + Object.keys(sample2)[i])
+      // console.log( Object.keys(sample)[i] + ' ----- ' + Object.keys(sample2)[i]) //keys
       // console.log(this.checkValue(Object.keys(sample)[i], Object.keys(sample2)[i]));
+
+
+      console.log(typeof sample[Object.keys(sample)[i]] + ' ----- '  + typeof sample[Object.keys(sample2)[i]]);
+
     }
 
+  }
+
+  checkType(type1, type2): boolean {
+    return type1 === type2;
   }
 
   checkValue(val1: string, val2: string): boolean {
