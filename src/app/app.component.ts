ca1082b70afb83b7d80c2a9660754845a9662c2d
kewo22 <kewinf271@gmail.com>
2019-05-08 18:29:01 +0530

    http read init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent implements OnInit {
   constructor(private http: HttpClient) {
     this.http.get("./sample.json").subscribe(data => {
       console.log(data);
+      
     });
   }
 
